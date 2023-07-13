119c66f94333b7a7d384ed8bbc25e2221e5403ee
patricklawton <plawt001@ucr.edu>
2023-07-13 09:54:18 -0700

    Added data processing and new plots

--- bifurcation_surface/cross_diffusion/project.py
+++ bifurcation_surface/cross_diffusion/project.py
@@ -6,6 +6,12 @@ from itertools import product
 
 project = sg.get_project()
 
+# Read in module info from shared data
+sd_fn = project.fn('shared_data.h5')
+with sg.H5Store(sd_fn).open(mode='r') as sd:
+    adj_mats = np.array(sd['adj_mats'])
+    modules = np.array([i.decode() for i in sd['modules']])
+
 def spherical_to_cartesian(ang_coord_sample):
     cart_vec = []
     for i in range(len(ang_coord_sample) + 1):
@@ -84,15 +90,28 @@ def generate_surface(job):
     #    data.update({cart_coord: []})
 
     # Sample n dimensional dispersal parameter space in (n-1) spherical coordinates
-    while len(data[ang_coords[0]]) < (2**len(C_k))*sp.N_n:
+    #while len(data[ang_coords[0]]) < (2**len(C_k))*sp.N_n:
+    if len(sp['C_ijs']) == 0:
+        total_samples = 3*sp.N_n
+    else:
+        total_samples = (2**(len(C_k)-3) + 3)*sp.N_n
+    while len(data[ang_coords[0]]) < total_samples:
         # Sample and store directional data
         ang_coord_sample = []
         for i, ang_coord in enumerate(ang_coords):
-            if i < len(ang_coords) - 1:
-                phi_range = (0.0, np.pi)
+            if len(sp['C_ijs']) == 0:
+                if i < len(ang_coords) - 1:
+                    phi_range = (np.pi/2, np.pi)
+                else:
+                    phi_range = (np.pi, 3*np.pi/2)
             else:
-                phi_range = (0.0, 2*np.pi)
-            phi_sample = phi_range[1] * np.random.sample()
+                if i <= len(sp['C_ijs']) - 1:
+                    phi_range = (0, np.pi)
+                elif i < len(ang_coords) - 1:
+                    phi_range = (np.pi/2, np.pi)
+                else:
+                    phi_range = (np.pi, 3*np.pi/2)
+            phi_sample = (phi_range[-1] - phi_range[0]) * np.random.sample() + phi_range[0]
             ang_coord_sample.append(phi_sample)
         for i, coord in enumerate(ang_coord_sample):
             data[ang_coords[i]].append(coord)
@@ -120,7 +139,6 @@ def generate_surface(job):
                 kappa_sts.append(float(sol))
             except:
                 kappa_sts.append(complex(sol))
-        # Store critical kappas
         for cond, kappa_arr in zip(['wav', 'st'], [kappa_wavs, kappa_sts]):
             kappa_c_dir = []
             for kappa_c in kappa_arr:
@@ -130,10 +148,10 @@ def generate_surface(job):
                     kappa_c_dir.append(np.nan)
             data['kappa_cs'][cond].append(kappa_c_dir)
 
-    # Store linear stability type (Omega integrand)
-    for i in range(len(data[ang_coords[0]])):
-        wav_nonan = [val for val in data['kappa_cs']['wav'][i] if not np.isnan(val)]
-        st_nonan = [val for val in data['kappa_cs']['st'][i] if not np.isnan(val)]
+        # Store linear stability type (Omega integrand)
+        #for i in range(len(data[ang_coords[0]])):
+        wav_nonan = [val for val in data['kappa_cs']['wav'][-1] if not np.isnan(val)]
+        st_nonan = [val for val in data['kappa_cs']['st'][-1] if not np.isnan(val)]
         wav = len(wav_nonan) > 0
         st = len(st_nonan) > 0
         if wav and st:
@@ -146,7 +164,7 @@ def generate_surface(job):
         data['omega_integrand']['st'].append(st_before_wav)
         data['omega_integrand']['stab'].append((wav == False) and (st == False))
 
-    # Store job data
+    # Store all job data
     for key, item in data.items():
         if isinstance(item, dict):
             for sub_key, sub_item in item.items():
@@ -156,6 +174,55 @@ def generate_surface(job):
     job.doc['surface_generated'] = True
     stop = timeit.default_timer()
     #print('Time:', stop - start)
+
+@FlowProject.pre(lambda job: job.doc.get('surface_generated'))
+@FlowProject.post(lambda job: job.doc.get('data_processed'))
+@FlowProject.operation
+def process_data(job):
+    with job.data:
+        C_ijs = list(job.sp['C_ijs'])
+        data_shape = [2 if i < (len(C_ijs) + 1) else 4 for i in range(len(C_ijs) + 2)]
+        omega_sec_mat = np.zeros(data_shape)
+        # Loop over parameter space sections
+        for phi_lims in product(*[list(range(dim)) for dim in data_shape]):
+            conds = []
+            for lim_i, lim in enumerate(phi_lims):
+                conds.append(np.array(job.data['phi_'+str(lim_i+1)]) > lim*np.pi/2)
+                conds.append(np.array(job.data['phi_'+str(lim_i+1)]) < (lim + 1)*np.pi/2)
+            # Find indices where angular coordinates are all within section ranges
+            cond = np.all(conds, axis=0)
+            if np.all(cond == False):
+                omega_sec_mat[phi_lims] = np.nan
+            else:
+                omega_integrand_sec = np.array(job.data['omega_integrand/wav'])[cond] + np.array(job.data['omega_integrand/st'])[cond]
+                omega_sec_mat[phi_lims] = sum(omega_integrand_sec) / sum(cond)
+        job.data['omega_sec_mat'] = omega_sec_mat
+        '''For now below only works for 1 cross element'''
+        if len(C_ijs) != 0:
+            adj = adj_mats[modules==job.sp.module][0]
+            i, j = C_ijs[0]
+            if adj[i,j] == 1.0:
+                # j feeds on i -> C_ij > 0
+                phi_lim_0 = 1
+            elif adj[j,i] == 1.0:
+                # i feeds on j -> C_ij < 0
+                phi_lim_0 = 0
+            else:
+                # Some special cases where C_ij != 0 for indirect interactions
+                if (job.sp.module == 'exploitative') and (C_ijs[0] in [[1,2], [2,1]]):
+                    # Negative interaction btwn predators in exploitative -> C_ij > 0
+                    phi_lim_0 = 1
+                elif (job.sp.module == 'apparent') and (C_ijs[0] in [[0,1], [1,0]]):
+                    # Negative interaction btwn prey in apparent -> C_ij > 0
+                    '''Not really sure about this assumption'''
+                    phi_lim_0 = 1
+                # For all cases besides those above, C_ij = 0
+                else:
+                    phi_lim_0 = np.nan
+            omega_constrained = omega_sec_mat[phi_lim_0,1,2] if not np.isnan(phi_lim_0) else np.nan
+            job.doc['omega_constrained'] = omega_constrained 
+            job.doc['omega_unconstrained'] = np.mean(omega_sec_mat, axis=0)[1,2]
+    job.doc['data_processed'] = True
 
 if __name__ == "__main__":
     FlowProject().main()
